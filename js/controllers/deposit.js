0ffe00fb90cddbef7c4c3173cba58a51f7b30215
Helghardt Avenant <helghardt@gmail.com>
2016-06-26 21:14:22 +0200

    Add deposit pages and connect endpoints.

--- js/controllers/deposit.js
+++ js/controllers/deposit.js
@@ -1,6 +1,41 @@
 angular.module('generic-client.controllers.deposit', [])
 
-    .controller('DepositCtrl', function ($scope) {
+    .controller('DepositCtrl', function ($scope, DepositDetails, TokenInfo) {
         'use strict';
-        $scope.data = {};
+        $scope.refreshDepositData = function () {
+            var getDepositDetails = DepositDetails.get();
+
+            getDepositDetails.success(
+                function (res) {
+                    $scope.items = res.data;
+                    $scope.deposit_reference = res.data[0].deposit_reference;
+                }
+            );
+
+            getDepositDetails.catch(function (error) {
+
+            });
+
+        };
+
+        $scope.refreshTokenInfo = function () {
+            var getTokenInfo = TokenInfo.get();
+
+            getTokenInfo.success(
+                function (res) {
+                    $scope.token = res.data;
+                }
+            );
+
+            getTokenInfo.catch(function (error) {
+
+            });
+
+        };
+
+        $scope.$on('$ionicView.afterEnter', function () {
+            $scope.refreshDepositData();
+            $scope.refreshTokenInfo();
+        });
+
     });
