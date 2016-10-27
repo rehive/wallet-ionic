ac6aebc51623e1c765537e1e86afd01f7082e444
odinsplasmarifle <generalaetius.jbvb@gmail.com>
2016-10-27 13:15:13 +0200

    Added notifcation controls for users on teh mobile app

--- www/js/controllers/notifications.js
+++ www/js/controllers/notifications.js
@@ -1,14 +1,47 @@
 angular.module('generic-client.controllers.notifications', [])
 
-    .controller('NotificationsCtrl', function ($scope) {
+
+
+    .controller('NotificationsCtrl', function ($scope, $ionicPopup, $ionicModal, $state, $ionicLoading, Notification) {
         'use strict';
         $scope.data = {};
-        $scope.notifications = [
-            {
-                "id": 1,
-                "title": "Updates",
-                "from": "Rehive",
-                "notification": "This feature is under development."
-            }
-        ]
+
+
+        $scope.list = function () {
+            Notification.list().success(
+                function (res) {
+                    // var items = [];
+
+                    // for (var i = 0; i < res.data.length; i++) {
+                    //     items.push(res.data[i]);
+                    //     console.log(res.data[i])
+                    // }
+
+                    $scope.notifications = res.data;
+                    // $window.localStorage.setItem('myBitcoinWithdrawalAccounts', JSON.stringify(items));
+                    // $scope.$broadcast('scroll.refreshComplete');
+                }
+            );
+        };
+
+        $scope.update = function (notification, email_enabled, sms_enabled) {
+            $ionicLoading.show({
+                template: 'Changing preferences...'
+            });
+
+            Notification.set(notification, email_enabled, sms_enabled).then(function (res) {
+                if (res.status === 200) {
+                    $ionicLoading.hide();
+                    $scope.list();
+                } else {
+                    $ionicLoading.hide();
+                    $ionicPopup.alert({title: "Error", template: res.message});
+                }
+            }).catch(function (error) {
+                $ionicPopup.alert({title: 'Authentication failed', template: error.message});
+                $ionicLoading.hide();
+            });
+        };
+
+        $scope.list();
     });
