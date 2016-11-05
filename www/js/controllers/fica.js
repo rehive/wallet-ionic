126c28dedcc3a6fb1769dc42ea2306483970a095
Helghardt Avenant <helghardt@gmail.com>
2016-11-05 15:02:26 -0700

    Revert changes.

--- www/js/controllers/fica.js
+++ www/js/controllers/fica.js
@@ -52,55 +52,20 @@ angular.module('generic-client.controllers.fica', [])
             $ionicLoading.show({
                 template: 'Uploading...'
             });
-
-
-            if (ionic.Platform.isIOS()) {
-                var options = {
-                    quality: 75,
-                    destinationType: Camera.DestinationType.DATA_URL,
-                    sourceType: Camera.PictureSourceType.CAMERA,
-                    allowEdit: true,
-                    encodingType: Camera.EncodingType.JPEG,
-                    targetWidth: 1024,
-                    targetHeight: 1024,
-                    popoverOptions: CameraPopoverOptions,
-                    saveToPhotoAlbum: true
-                };
-
-
-                Upload.upload({
-                    //url: API + '/image/',
-                    //fileName: 'image.jpeg',
-                    //fileFormDataName: 'image',
-                    //file: imageData
-                    url: API + "/users/document/",
-                    data: {file: file, document_category: "", document_type: ""},
-                    headers: {'Authorization': 'JWT ' + Auth.getToken()}
-                }).progress(function (evt) {
-                    var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-                    console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
-                }).success(function (data, status, headers, config) {
-                    $ionicPopup.alert({title: "Document Uploaded"});
-                }).error(function (data, status, headers, config) {
-                    alert('error status: ' + status);
-                });
-
-            } else {
-                Upload.upload({
-                    url: API + "/users/document/",
-                    data: {file: file, document_category: "", document_type: ""},
-                    headers: {'Authorization': 'JWT ' + Auth.getToken()}
-                }).then(function (resp) {
-                    $ionicLoading.hide();
-                    $ionicPopup.alert({title: "Success", template: "Upload complete."});
-                    console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
-                }, function (resp) {
-                    console.log('Error status: ' + resp.status);
-                }, function (evt) {
-                    $ionicLoading.hide();
-                    var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-                    console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
-                });
-            }
+            Upload.upload({
+                url: API + "/users/document/",
+                data: {file: file, document_category: "", document_type: ""},
+                headers: {'Authorization': 'JWT ' + Auth.getToken()}
+            }).then(function (resp) {
+                $ionicLoading.hide();
+                $ionicPopup.alert({title: "Success", template: "Upload complete."});
+                console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+            }, function (resp) {
+                console.log('Error status: ' + resp.status);
+            }, function (evt) {
+                $ionicLoading.hide();
+                var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+            });
         };
     });
